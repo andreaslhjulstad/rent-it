090483c24d97f9e4e03179a1e53306d1c297ffb3
Andreas Lilleby Hjulstad <andrelhj@stud.ntnu.no>
2023-02-10 16:32:34 +0100

    Add ads collection and add to LocalData
    Relates: #9

--- app/src/Data/LocalData.ts
+++ app/src/Data/LocalData.ts
@@ -1,5 +1,7 @@
 import { UsersCollection } from "./Users/UsersCollection";
+import { AdsCollection } from "./Ads/AdsCollection";
 
 export class LocalData {
   static users = new UsersCollection();
+  static ads = new AdsCollection();
 }
