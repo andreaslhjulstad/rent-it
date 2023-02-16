42312415e6a3ba7e7eee03546c9ec35980e2f092
Synne Borge <synneborge@dhcp-10-22-43-57.wlan.ntnu.no>
2023-02-16 09:31:35 +0100

    AdPage update

--- app/src/Pages/ViewAdPage/AdPage.tsx
+++ app/src/Pages/ViewAdPage/AdPage.tsx
@@ -1,21 +1,63 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./AdPage.module.css";
-import buttonStyles from "../../../GlobalStyling/Buttons.module.css";
+import buttonStyles from "../../GlobalStyling/Buttons.module.css";
 import { LocalData } from "../../Data/LocalData";
 import { AdsData } from "../../Data/Ads/AdsData";
-
+import { getAuth } from "firebase/auth";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 export const AdPage = () => {
+  const navigate = useNavigate();
+
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
+  // const [price, setPrice] = useState(0);
+  // const [images, setImages] = useState<File[]>([]);
+  // const [area, setArea] = useState("");
+
+
     let id = "n8V2nOv66r7dj4sxFvLe";
     let doc = new AdsData(id)
     doc.load().then(() => {
         
     })
+
+    const rentIt = () => {
+
+    }
+
     return (
-      <div>
-        <p>Du er logget inn:)</p>
-        <Link to="/createAd">Opprett annonse</Link>
+      <div id={styles.adPage}>
+          <div className={styles.frameRoot}>
+              <form className={styles.rentItButton} onSubmit={rentIt}>
+                <img className={styles.image1} src="Hammer.jpg" alt="Hammer"></img>
+                <div className={styles.descriptionContainer}>
+                  <div className={styles.text2}> Hammer </div>
+                  <div className={styles.text1}> -Besrkivelse </div>
+                </div>
+                <div className={styles.infoContainer}>
+                    <div className={styles.userAndTitle}>
+                        <div className={styles.text1}> Område:  </div>
+                        <div className={styles.text2}> Sted </div>
+                    </div>
+                    <div className={styles.CurrentPrice}>
+                        <div className={styles.text1}> Pris: </div>
+                        <div className={styles.text2}> 20 kr </div>
+                    </div>
+                </div>
+                <button type="submit" className={buttonStyles.rentItButton}>
+                    Rent it
+                </button>
+                <div className={styles.text3}> Kontaktinformasjon 
+                  <div className={styles.text1}> Navn:  </div>
+                  <div className={styles.text1}> tlf:  </div>
+                  <div className={styles.text1}> e-mail: </div>
+                </div>
+
+              </form>
+
+          </div>
       </div>
     )
   };
