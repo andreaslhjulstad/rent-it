fef5fa8c38cb4306c7b164d60d433ad5a839d61a
Synne Borge <synneborge@dhcp-10-22-8-166.wlan.ntnu.no>
2023-02-22 12:38:44 +0100

    singelAdTests

--- app/src/Pages/ViewAdPage/AdPage.test.tsx
+++ app/src/Pages/ViewAdPage/AdPage.test.tsx
@@ -1,32 +1,43 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen, waitFor, within } from "@testing-library/react";
 import AdPage from "./AdPage";
+import { MemoryRouter } from "react-router-dom";
 
-test("elements are rendered", () => {
-    render(<AdPage />); //Laster inn siden
-
-    //Henter elementene fra siden som ikke er firebase-data
-    const area = screen.getByRole("textbox", {name: / Område: /i, });
-    const price = screen.getByRole("textbox", {name: / Pris: /i,  });
-    const button = screen.getByRole("button", {name: / Rent it /i, }); 
-    const contactInfo = screen.getByRole("textbox",{name: /Kontaktinformasjon:/i, });
-    const goTo = screen.getByRole("textbox",{name: /Gå til bruker:/i,  });
-    const userLink = screen.getByRole("link", {name: /Ola Nordmann/i,  });
-    const tlf = screen.getByRole("textbox", {name: /tlf:/i, });
-    const email = screen.getByRole("textbox",{name: /e-mail:/i, });
-
-    //Tester at elementene er på siden
-    expect(area).toBeInTheDocument();
-    expect(price).toBeInTheDocument();
-    expect(button).toBeInTheDocument(); 
-    expect(contactInfo).toBeInTheDocument();
-    expect(goTo).toBeInTheDocument();
-    expect(userLink).toBeInTheDocument();
-    expect(tlf).toBeInTheDocument();
-    expect(email).toBeInTheDocument();
+describe("AdPage", () => {
+    const setup = async () => {
+      render(
+        <MemoryRouter>
+          <AdPage />
+        </MemoryRouter>
+      );
+      await waitFor(() => {
+        const { getByText } = within(screen.getByTestId("adPage"));
+        expect(getByText("Område:")).toBeInTheDocument();
+      });
+    };
+    test("elements are rendered", () => {
+        setup(); //Laster inn siden
+        //Henter elementene fra siden som ikke er firebase-data
+        //Fire-base datainnlasting valideres i AdData.ts
+        const area = screen.getByText("Område:");
+        const price = screen.getByText("Pris:");
+        const button = screen.getByText("Rent it"); 
+        const contactInfo = screen.getByText("Kontaktinformasjon")
+        const goTo = screen.getByText("Gå til bruker:");
+        const tlf = screen.getByText("tlf:");
+        const email = screen.getByText("e-mail:");
+        //Tester at elementene er på siden
+        expect(area).toBeInTheDocument();
+        expect(price).toBeInTheDocument();
+        expect(button).toBeInTheDocument(); 
+        expect(contactInfo).toBeInTheDocument();
+        expect(goTo).toBeInTheDocument();
+        expect(tlf).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+    })
     
-    
-    
-    
-    
-
-})
+    test("being able to submit the form to rent it", () => {
+        setup();
+        const submitButton = screen.getByRole("button");
+        expect(submitButton).toBeInTheDocument();
+      });
+});
