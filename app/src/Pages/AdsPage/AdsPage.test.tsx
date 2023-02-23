3446f11ff0cda8980a7051e91d9d02a9ade4868b
Herman Sætre <hermasa@stud.ntnu.no>
2023-02-23 19:50:38 +0100

    #15 cleanup

--- app/src/Pages/AdsPage/AdsPage.test.tsx
+++ app/src/Pages/AdsPage/AdsPage.test.tsx
@@ -29,33 +29,4 @@ import firestorePkg, { DocumentData, DocumentReference, FirestoreDataConverter} 
         expect(grid).toBeInTheDocument();
 
     });
-
-    test("render AddBox", () => {
-        //Her må man lage et "fake" firestore doc men mocReturn value er bare ikke enkel å sammarbeide med
-        
-        /* let docSpy = jest.spyOn(firestorePkg, 'doc').mockReturnValue(
-            {   // Jeg vet ikke hva som skal være på parent og withConverter
-                converter: null,
-                type: "document",
-                id: "",
-                path: "",
-                parent: undefined,
-                withConverter: function <U>(converter: FirestoreDataConverter<U>): DocumentReference<U> {
-                    throw new Error("Function not implemented.");
-                }
-            }    
-        );
-
-        let useDocSpy = jest.spyOn(firestorePkg, 'useFirestoreDocument').mockReturnValue( //Jeg aner ikke hvorfor det er feil
-            { data: { title: "test", area: "her", description: "Test12", price: 12, images:"images/ads/23QEH97wCaVqEzxCEutY/bosch-universalimpact-18v-60-drill-screwdriver-battery-included.jpg", userId: "vNjSarNvWwbYWoaeCtR43NfHf7C2"} }
-        )
-
-        let app = render(<AdsPage />)
-        let title = app.getByText(/test/i)
-        expect(title).toBeInTheDocument()
-
-        docSpy.mockRestore()
-        useDocSpy.mockRestore() */
-
-    });
 })
