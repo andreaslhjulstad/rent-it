fac68b81d06414d9448855093d3d725d2fd13f39
Andreas Lilleby Hjulstad <andrelhj@stud.ntnu.no>
2023-03-01 19:31:27 +0100

    Add new LoanAgreementData class and add it to LocalData
    Relates: #59

--- app/src/Data/LocalData.ts
+++ app/src/Data/LocalData.ts
@@ -2,10 +2,12 @@ import { UserData } from "./Users/UserData";
 import { getAuth } from "firebase/auth";
 import CollectionLoader from "./CollectionLoader";
 import { AdData } from "./Ads/AdData";
+import { LoanAgreementData } from "./LoanAgreements/LoanAgreementData";
 
 export class LocalData {
   static users = new CollectionLoader("users", undefined, UserData);
   static ads = new CollectionLoader("ads", undefined, AdData);
+  static loanAgreements = new CollectionLoader("loanAgreements", undefined, LoanAgreementData);
 
   static currentUser?: UserData;
 
